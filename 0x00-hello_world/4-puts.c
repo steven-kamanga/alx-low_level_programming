4999e5e3871c2372b162fc52f80cc03113bfe607
drplxrd <kamangasteven00@gmail.com>
2022-12-11 23:08:38 -0800

    Before advanced stuff fix2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-int main(void){
+
+int main(void)
+{
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte()s\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
