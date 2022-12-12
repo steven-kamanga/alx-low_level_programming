6396eb96c8e54a205bb5e0c1d6471f666a1b94e8
drplxrd <kamangasteven00@gmail.com>
2022-12-11 23:21:39 -0800

    imma quit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  *main - Entry point
  *
@@ -7,6 +8,7 @@
  *Return: Always 0 (Success)
  * 
 */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  *
  *Description: using sizeof to print the size of various types
  *
- *Retur: Always 0 (Successful)
+ *Return: Always 0 (Successful)
 */
 int main(void)
 {
@@ -12,6 +12,6 @@ int main(void)
 	printf("Size of an int: %lu byte()s\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("Size of float: %lu byte(s)\n", sizeof(float));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
