c7a3ccfc8056554f1444e777c585b1d5212e6179
drplxrd <kamangasteven00@gmail.com>
2022-12-14 02:05:20 -0800

    debuggimh

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *main - Entry point
+ *
+ *Return : 0
+ *
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 98 % 2;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+Some more low level drip
