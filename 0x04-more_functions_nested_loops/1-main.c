0d620be5840d9031e24b7473e2ba437e2a96e49a
drplxrd <kamangasteven00@gmail.com>
2022-12-15 13:58:20 +0200

    more files

---
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Check code for ALX School Students
+ * 
+ * Return: ALways 0 (Success)
+ * 
+*/
+
+int main(void)
+{
+    char c;
+
+    c = '0';
+    printf("%c: %d\n", c, _isdigit(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isdigit(c));
+    return (0);
+}
