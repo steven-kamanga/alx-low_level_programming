588ac7613ef816170716ebc0535fa3cd6ead183c
drplxrd <kamangasteven00@gmail.com>
2022-12-22 19:22:53 +0200

    more

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,33 +1,31 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
-  * rot13 - ...
-  * @s: ...
-  *
-  * Return: ...
-  */
-char *rot13(char *s)
+ * rot13 - Write a function that encodes a string using rot13
+ *
+ * @hi: This is my input string
+ *
+ * Return: String converted to rot13
+ *
+ */
+
+char *rot13(char *hi)
 {
-int a = 0;
+	int index, count;
 
-while (s[a])
-{
-while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A' && s[a] <= 'Z'))
-{
-if ((s[a] > 'm' && s[a] <= 'z') || (s[a] > 'M' && s[a] <= 'Z'))
-{
-s[a] -= 13;
-break;
+	char minus[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char mayus[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (index = 0; hi[index] != '\0'; ++index)
+	{
+		for (count = 0; minus[count] != '\0' ; count++)
+		{
+			if (hi[index] == minus[count])
+			{
+				hi[index] = mayus[count];
+				break;
+			}
+		}
+	}
+	hi[index] = '\0';
+	return (hi);
 }
-
-s[a] += 13;
-break;
-}
-
-a++;
-}
-
-return (s);
-}
-
