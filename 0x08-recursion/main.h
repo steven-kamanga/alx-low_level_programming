90b331ed7144b6cc3c414e3dce7614e002c516a4
drplxrd <kamangasteven00@gmail.com>
2023-01-04 23:04:36 +0200

    recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+
+#endif /* HOLBERTON_H */
