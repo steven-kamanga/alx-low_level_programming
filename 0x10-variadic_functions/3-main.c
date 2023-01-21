12c0d254aa3f49c7191098d233c2db2950a82c8d
drplxrd <kamangasteven00@gmail.com>
2023-01-22 00:53:41 +0200

    variadic functions

---
+++ 0x10-variadic_functions/3-main.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	print_all("ceis", 'H', 0, "lberton");
+	print_all("fcsi", 3.14435, 'H', NULL, 402);
+	return (0);
+}
