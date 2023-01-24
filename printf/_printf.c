9b08a42c88ddb79fed0adfb56ef486ea408ea3f9
drplxrd <kamangasteven00@gmail.com>
2023-01-25 00:05:17 +0200

    Alter Print f

--- printf/_printf.c
+++ printf/_printf.c
@@ -5,9 +5,6 @@
 /**
  * _printf - produces output according to a format
  * @format: format string containing the characters and the specifiers
- * Description: this function will call the get_print() function that will
- * determine which printing function to call depending on the conversion
- * specifiers contained into fmt
  * Return: length of the formatted output string
  */
 int _printf(const char *format, ...)
