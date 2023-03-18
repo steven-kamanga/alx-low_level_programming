66ed5c50de0e7a7cd2a1777b064637886458cb40
drplxrd <kamangasteven00@gmail.com>
2023-03-18 15:21:11 +0200

    more

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,21 +1,20 @@
-#include <lists.h>
+#include "lists.h"
 
 /**
  * dlistint_len - Return list len
  * @h: struct list
- * Return: list len
+ * Return: list_len
  */
 
- size_t dlistint_len(const dlistint_t *h)
- {
-     size_t len = 0;
-     int i = 0;
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t len = 0;
+	int i = 0;
 
-     for (i = 0; h; i++)
-     {
-        len += 1;
-        h = h->next;
-     }
-     return len;
- }
- 
+	for (i = 0; h; i++)
+	{
+		len += 1;
+		h = h->next;
+	}
+	return (len);
+}
