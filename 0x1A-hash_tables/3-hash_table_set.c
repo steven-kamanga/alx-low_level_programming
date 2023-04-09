0711ef85e79536420fec2737195e564d079c98bf
drplxrd <kamangasteven00@gmail.com>
2023-04-09 22:54:12 +0200

    Fixed betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,7 +18,6 @@ hash_node_t *make_hash_node(const char *key, const char *value)
 	if (node->key == NULL)
 	{
 		free(node);
-
 		return (NULL);
 	}
 	node->value = strdup(value);
@@ -26,7 +25,6 @@ hash_node_t *make_hash_node(const char *key, const char *value)
 	{
 		free(node->key);
 		free(node);
-
 		return (NULL);
 	}
 	node->next = NULL;
@@ -42,41 +40,38 @@ hash_node_t *make_hash_node(const char *key, const char *value)
  *
  * Return: 1 if successful, 0 otherwise
  */
-
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
-    hash_node_t *node, *tmp;
+	unsigned long int index;
+	hash_node_t *node, *tmp;
 
-    if (ht == NULL || key == NULL || value == NULL || *key == '\0')
-        return (0);
-    index = key_index((const unsigned char *)key, ht->size);
-    node = make_hash_node(key, value);
-    if (node == NULL)
-        return (0);
-    if (ht->array[index] == NULL)
-    {
-        ht->array[index] = node;
+	if (ht == NULL || key == NULL || value == NULL || *key == '\0')
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	node = make_hash_node(key, value);
+	if (node == NULL)
+		return (0);
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = node;
+		return (1);
+	}
+	tmp = ht->array[index];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			free(node->key);
+			free(node->value);
+			free(node);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+	node->next = ht->array[index];
+	ht->array[index] = node;
 
-        return (1);
-    }
-    tmp = ht->array[index];
-    while (tmp != NULL)
-    {
-        if (strcmp(tmp->key, key) == 0)
-        {
-            free(tmp->value);
-            tmp->value = strdup(value);
-            free(node->key);
-            free(node->value);
-            free(node);
-            
-            return (1);
-        }
-        tmp = tmp->next;
-    }
-    node->next = ht->array[index];
-    ht->array[index] = node;
-
-    return (1);
+	return (1);
 }
