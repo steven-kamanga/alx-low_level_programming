c7a442a7fbb9dd40cfde28de0ddc957f532c7836
steven-kamanga <kamangasteven00@gmail.com>
2024-03-05 18:30:18 +0200

    endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	int num = 1;
+	char *endian = (char *)&num;
+
+	if (*endian == 1)
+		return (1);
+	return (0);
+}
