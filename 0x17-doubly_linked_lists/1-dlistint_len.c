ea0354df711d34de127c76d216149fb2b01edb9f
steven-kamanga <kamangasteven00@gmail.com>
2024-03-23 07:24:10 +0200

    fixed typos and prototypes

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,14 +2,14 @@
 #include "lists.h"
 
 /**
- * dlisting_len - Function that returns
+ * dlistint_len - Function that returns
  * the number or elements in a linked list
  * @h: pointer to the list
  * Return: number of elements in a linked list
  *
 */
 
-size_t dlisting_len(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	int count = 0;
 
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -18,5 +18,7 @@ struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+size_t dlistint_len(const dlistint_t *h);
 
 #endif /* DLISTINT_H */
